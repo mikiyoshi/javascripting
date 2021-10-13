ebd1384c9cb6ab70ee45969be0e96b3266c35b2c
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-10-13 02:39:55 +0000

    this file updated for objects trash to treasure challenge

---
+++ objects-trash-to-treasure.js
@@ -0,0 +1,21 @@
+function smartGarbage(trash, bins) {
+  if (bins.waste >= 0 || bins.recycling >= 0 || bins.compost >= 0) {
+  } else {
+    bins.waste = 0;
+    bins.recycling = 0;
+    bins.compost = 0;
+  }
+  if (trash === "waste") {
+    bins.waste++;
+    console.log(bins.waste);
+  } else if (trash === "recycling") {
+    bins.recycling++;
+    console.log(bins.recycling);
+  } else if (trash === "compost") {
+    bins.compost++;
+    console.log(bins.compost);
+  }
+  return bins;
+}
+smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
+smartGarbage('compost', { waste: 0, recycling: 5, compost: 0 });
